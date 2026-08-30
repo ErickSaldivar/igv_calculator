7f81fec17d6095e538419c84e3d1064dcfa56d47
ErickSaldivar <119002725+ErickSaldivar@users.noreply.github.com>
2026-08-30 12:12:43 -0500

    chore: update gradle 9.1, agp 9.0.1, kotlin 2.3.20, targetSdk 36 and bump version to 2.0.1+8

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,18 +16,20 @@ if (keystorePropertiesFile.exists()) {
 
 
 
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
+}
+
 android {
     namespace = "com.nadsat.igv_calculator"
     compileSdk = 36
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     defaultConfig {
@@ -35,7 +37,7 @@ android {
         applicationId = "com.nadsat.igv_calculator"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 24
+        minSdk = 30
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
